c3165adcf8fb37fb50f3acedeac2522bfe6ac0f0
Damir Dobric <ddobric@daenet.de>
2017-05-15 21:45:59 +0200

    Update Program.cs

--- AadTokenGen/Program.cs
+++ AadTokenGen/Program.cs
@@ -16,14 +16,7 @@ namespace AadTokenGen
 
         private static string createToken(string userName, string clientId, string resource, string redirectUri, string authority = null)
         {
-            //string authority = "3b0f78fd-01d5-4c43-a2ae-3a6f6b8cabe7"; // tenant
-                                                                       // string resource = "http://iottsystems/mywerkservice.de"; // "027d4e8d-338a-4dfa-85da-269a43849137" This is the client id of the service.
-                                                                       //string clientId = "7315c7e4-473f-4734-8f34-c2e408dd2aed";
-                                                                       //string redirectUri = "https://localhost:63803/";
-
             AuthenticationContext authContext;
-
-            //http://www.cloudidentity.com/blog/2014/07/08/using-adal-net-to-authenticate-users-via-usernamepassword/
 
             if (authority == null)
                 authContext = new AuthenticationContext(string.Format("https://login.microsoftonline.com/{0}", authority));
@@ -41,6 +34,5 @@ namespace AadTokenGen
 
             return aHeader;
         }
-
     }
 }
